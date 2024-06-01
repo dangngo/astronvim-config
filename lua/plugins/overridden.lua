8193a51a6d532b3fdeb7bf9152d2cbe45795fc9f
Dang Ngo <ngominh.dang96@gmail.com>
2024-06-01 17:18:31 +0700

    nit: default telescope theme and set colorscheme

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -1,7 +1,38 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = function(_, opts)
-    opts.sources = { "filesystem" }
-    opts.filesystem.filtered_items.visible = true
-  end,
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.sources = { "filesystem" }
+      opts.filesystem.filtered_items.visible = true
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      opts.pickers = {
+        colorscheme = {
+          enable_preview = true,
+        },
+        find_files = {
+          theme = "ivy",
+          hidden = true,
+        },
+        buffers = {
+          theme = "ivy",
+        },
+        live_grep = {
+          theme = "ivy",
+        },
+        current_buffer_fuzzy_find = {
+          theme = "ivy",
+        },
+        oldfiles = {
+          theme = "ivy",
+        },
+        registers = {
+          theme = "cursor",
+        },
+      }
+    end,
+  },
 }
